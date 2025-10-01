5858f22f78fa38edd58e0a53e960723bc7ac43ea
Anastasia <pidanla6@gmail.com>
2025-09-30 17:07:40 -0800

    Started script but also added mkdocs section

---
+++ scripts/browsing_dfs.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+
+def import_grace_smb():
+    pass
+
+def import_mankoff_ice_discharge():
+    pass
+
+def import_runoff():
+    pass 
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
