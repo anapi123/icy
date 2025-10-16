6f2b2f9b4ebbddd468262a086fe4929ca94ff04b
Anastasia <pidanla6@gmail.com>
2025-10-15 22:30:00 -0800

    Successful creating of mini dfs, now have to drop nans probably before concatenating cause getting an error which has to do with the time indices, in that probably the NaTs or there's duplicate timestamps?

--- scripts/browsing_dfs.py
+++ scripts/browsing_dfs.py
@@ -1,46 +1,69 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# todo get 1) temperature data in RACMO and 2) precip data in RACMO 
+# todo get 1) temperature data in RACMO and 2) precip data in RACMO
 # percent ice runoff versus surface discharge (see Miaja's COLAB)
 
 # I am purple (SW)
-# RACMO: runoff, temperature, precipitation; GRACE: mass balance; Mankoff: ice discharge 
+# RACMO: runoff, temperature, precipitation; GRACE: mass balance; Mankoff: ice discharge
 
-def _import_file(filename: str, 
-                 **kwargs) -> pd.DataFrame:
+
+def _import_file(filename: str, date_format: str, **kwargs) -> pd.DataFrame:
     """Import one of the data files - to be used for grace and ice discharge because those dfs don't have two date columns (Year and Month)
 
     Args:
-        filename (str): 
+        filename (str):
 
     Returns:
-        pd.DataFrame: 
+        pd.DataFrame:
     """
     df = pd.read_csv(
         f"/home/achen7/icy/data/{filename}",
         delimiter=",",
+        date_format=date_format,
         na_values=["NaN", "NAN", "NaN", "None", "nan", "99999.9"],
-        **kwargs
-        )
-    
+        **kwargs,
+    )
+
     return df
 
-    
-def _format_date(df: pd.DataFrame,
-                 date_column_name: str) -> pd.DataFrame:
+
+def _format_date(df: pd.DataFrame, date_column_name: str) -> pd.DataFrame:
     """Format an 'Year' and 'Month' column into a new column named to ex) 'Date' where the datetime format is ex) 2002-04-01, so %Y-%m-%d
 
     Args:
-        df (pd.DataFrame): 
-        date_column_name (str): 
+        df (pd.DataFrame):
+        date_column_name (str):
 
     Returns:
-        pd.DataFrame: 
+        pd.DataFrame:
     """
     df[date_column_name] = pd.to_datetime(df[["Year", "Month"]].assign(DAY=1))
-    
+
     return df
+
+
+def _smol_df(df: pd.DataFrame,
+                date_column_name: str,
+                measurement_column: str,
+                renamed_column: str) -> pd.DataFrame:
+    """Pull the date and the measurement column only. Then renames the measurement column to something descriptive. Then sets time column as index. Assumes that the date has already been formatted into %Y-%m-%d.
+
+    Args:
+        df (pd.DataFrame): Raw df, altered with an additional date column which puts Year and Month column into one date column with %Y-%m-%d format.
+        date_column_name (str): The column that is already manipulated into %Y-%m-%d format.
+        measurement_column (str): ex) for RACMO dfs- mine is "SW" column
+        renamed_column (str): ex) "Date"
+
+    Returns:
+        pd.DataFrame:
+    """
+    df_smol = df[[date_column_name, measurement_column]]
+    df_smol = df_smol.rename(columns={measurement_column: renamed_column})
+    df_smol.set_index(date_column_name, inplace=True)
+    
+    return df_smol
+    
     
 def import_grace_smb() -> pd.DataFrame:
     """Changes the original Year-Month date format into a 'Date' column in %Y-%m-%d format and sets that as the time index. Other column is 'grace_gt_month'
@@ -50,96 +73,138 @@ def import_grace_smb() -> pd.DataFrame:
     """
     mass_balance_df = _import_file(
         filename="averaged_GRACE_GMB_basin_gigatons_month_mass_balance.csv",
-        date_format="%Y-%b", 
-        parse_dates=" - Year-Month",
+        date_format="%Y-%b",
+        parse_dates=[" - Year-Month"],
     )
-    
-    # Create an Year column and a Month column 
+
+    # Create an Year column and a Month column
     mass_balance_df["Year"] = mass_balance_df[" - Year-Month"].dt.year
     mass_balance_df["Month"] = mass_balance_df[" - Year-Month"].dt.month
-    # Create final time column in %Y-%m-%d format 
-    mass_balance_df["Date"] = pd.to_datetime(mass_balance_df[["Year", "Month"]].assign(DAY=1))
+    # Create final time column in %Y-%m-%d format
+    mass_balance_df = _format_date(df=mass_balance_df,
+                                   date_column_name="Date")
     # Drop the previous - Year-month column and extra created Year and Month columns
     mass_balance_df = mass_balance_df.drop(" - Year-Month", axis=1)
     mass_balance_df = mass_balance_df.drop("Year", axis=1)
     mass_balance_df = mass_balance_df.drop("Month", axis=1)
-    
-    # Extract final date and my sw column into mini df and rename that sw column
-    grace_df = mass_balance_df[["Date", "AVERAGE of SW"]]
-    grace_df = grace_df.rename(columns={"AVERAGE of SW": "grace_gt_month"})
-    # Set date column as index
-    grace_df.set_index("Date", inplace=True)
-    
+
+    grace_df = _smol_df(
+        df=mass_balance_df,
+        date_column_name="Date",
+        measurement_column="AVERAGE of SW",
+        renamed_column="sw_grace_gt_month",
+    )
+
     return grace_df
 
+
 # Plot data 2009-2018 cause that's where the other basins have all data. # todo If I have gaps let group know
-# 10-10 I found out today I have 500 duplicated timestamps which are the NaTs so #FIXME drop the nans 
+# 10-10 I found out today I have 500 duplicated timestamps which are the NaTs so #FIXME drop the nans
 def import_mankoff_ice_discharge() -> pd.DataFrame:
-    """Creates mini df with 'Date' column set as index in %Y-%m-%d format and other column is 'ice_discharge_gt_month'
+    """Creates mini df with 'Date' column set as index in %Y-%m-%d format and other column is "sw_avg_ice_disch_gt_month"
 
     Returns:
-        pd.DataFrame: 
+        pd.DataFrame:
     """
     ice_discharge_df = _import_file(
         filename="Mankoff_region_D_Gt_month-1_ice_discharge_avg.csv",
         date_format="%m/%d/%y",
-        parse_dates="Date",
+        parse_dates=["Date"],
     )
-    
-    # Convert original Date column to datetime time cause it is object type rn for some reason 
+
+    # Convert original Date column to datetime time cause it is object type rn for some reason
     ice_discharge_df["Date"] = pd.to_datetime(ice_discharge_df["Date"])
-    
-    # Extract date and 'AVERAGE OF SW' column
-    ice_discharge_df_mini = ice_discharge_df[["Date", "SW"]]
-    # Rename average of sw column 
-    ice_discharge_df_mini = ice_discharge_df_mini.rename(columns={"SW" : "ice_discharge_gt_month"})    
-    # Set Date column as time index
-    ice_discharge_df_mini.set_index("Date", inplace=True)
-    
+
+    ice_discharge_df_mini = _smol_df(df=ice_discharge_df,
+                                     date_column_name="Date",
+                                     measurement_column="SW",
+                                     renamed_column="sw_avg_ice_disch_gt_month")
+
     return ice_discharge_df_mini
 
-def import_runoff()-> pd.DataFrame:
+
+def import_runoff() -> pd.DataFrame:
+    """SW runoff gt/month, "Date" and "sw_runoff_gt_month" columns. Dates formatted %Y-%m-%d.
+
+    Returns:
+        pd.DataFrame:
+    """
     runoff_df = _import_file(
         filename="RACMO_runoff_gigatons_per_month.csv",
         date_format="%Y",
-        parse_dates="Year",
     )
+
+    runoff_df = _format_date(df=runoff_df,
+                             date_column_name="Date")
     
-    print("runoff")
-    print(runoff_df.columns)
-    print(runoff_df["Year"])
-    print(runoff_df["Month"])
-    # Format 
+    runoff_df_mini = _smol_df(df=runoff_df,
+                              date_column_name="Date",
+                              measurement_column="SW",
+                              renamed_column="sw_runoff_gt_month")
     
-    return runoff_df
+    return runoff_df_mini
+
 
 def import_ppt() -> pd.DataFrame:
+    """SW sum ppt in gigatons for each month, "Date" and "sw_sum_ppt_gt_each_month" columns. Dates formatted %Y-%m-%d.
+
+    Returns:
+        pd.DataFrame: 
+    """
     ppt_df = _import_file(
         filename="RACMOdataset_monthly_PRECIP_sum_(Gt).csv",
-        date_format="%Y",
-        parse_dates="Year",
+        date_format="%Y"
+    )
+    
+    ppt_df = _format_date(df=ppt_df, 
+                          date_column_name="Date")
+    ppt_df_mini = _smol_df(
+        df=ppt_df,
+        date_column_name="Date",
+        measurement_column="SW",
+        renamed_column="sw_sum_ppt_gt_each_month",
     )
 
-    return ppt_df
+    return ppt_df_mini
+
 
 def import_temp_2m() -> pd.DataFrame:
+    """SW avg air temp 2m above surface in K for each month. "Date" and "sw_monthly_avg_temp_2m" columns. Dates formatted %Y-%m-%d.
+
+    Returns:
+        pd.DataFrame:
+    """
     temp_df = _import_file(
         filename="RACMOdataset_monthly_T(air 2m above surface)_avg_(K).csv",
-        date_format="%Y",
-        parse_dates="Year",
+        date_format="%Y"
+    )
+    
+    temp_df = _format_date(df=temp_df, date_column_name="Date")
+    temp_df_mini = _smol_df(
+        df=temp_df,
+        date_column_name="Date",
+        measurement_column="SW",
+        renamed_column="sw_monthly_avg_temp_2m",
     )
 
-    return temp_df
+    return temp_df_mini
 
-def main():
+
+def concat_dfs() -> pd.DataFrame:
     mass_balance_df = import_grace_smb()
     ice_discharge_df = import_mankoff_ice_discharge()
     runoff_df = import_runoff()
     ppt_df = import_ppt()
     temp_df = import_temp_2m()
+
+    concat_df = pd.concat((df for df in [mass_balance_df, ice_discharge_df, runoff_df, ppt_df, temp_df]), axis=1)
     
-    print(mass_balance_df)
-    
+    return concat_df
+
+def main():
+    concat_df = concat_dfs()
+    print(concat_df)
+
 
 if __name__ == "__main__":
     main()
