2f926d1bdd73625fbf363484f99ce1f25f5319f3
Anastasia <pidanla6@gmail.com>
2025-11-01 18:58:18 -0800

    Fixed date parse bug which made scatterplots all vertical lines stacked, and normalized total loss vals by area

--- scripts/indiv_prelim_plots.py
+++ scripts/indiv_prelim_plots.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 from create_big_df import import_file
 
-from plot_styling import fontsize_style, scatter_style
+from plot_styling import fontsize_style
 from splots.saxes import thic_plot
 
 
@@ -15,7 +15,7 @@ def _import_total_loss() -> pd.DataFrame:
     """
     total_loss_df = import_file(
         filename="Final_Plots_Datasets - Total_loss (not normalized; runoff + ice discharge.csv",
-        date_format="%M/%d/%Y",
+        date_format="%m/%d/%Y",
         parse_dates=["Date"],
     )
 
@@ -33,32 +33,42 @@ def plot_total_loss():
     total_loss_df = _import_total_loss()
 
     fontstyle = fontsize_style()
-    scatterstyle = scatter_style()
 
     # Col map with color
     region_color = {
         "SE": "#f23f5d",
         "E+W": "#4bf288",
         "N+NW": "#4fadff",
-        "NE": "#e6a030",
+        "NE": "#ffbe0d",
         "SW": "#afa1ff",
+    }
+    # Area of each basin- normalize total loss by area of each region 
+    region_area = {
+        "SE": 165348.6113,
+        "E+W": 662182.9852,
+        "N+NW": 547172.7864,
+        "NE": 425242.1042,
+        "SW": 216207.285
     }
 
     fig, ax = plt.subplots(nrows=1, ncols=1)
 
     for col, pnt_color in region_color.items():
         ax.scatter(
-            total_loss_df.index.values,
-            total_loss_df[col],
+            total_loss_df.index,
+            total_loss_df[col]
+            / region_area[
+                col
+            ] * 10**3,  # Normalize total loss by area to make values more comparable and make Gt into Mt 
             s=200,
             edgecolor="white",
             linewidth=0.01,
             c=pnt_color,
-            label=col
+            label=col,
         )
     legend = ax.legend(fontsize=15)
     
-    ax.set_ylabel("Total Loss GrIS / (Gt $\\cdot$ month$^{-1}$))", **fontstyle)
+    ax.set_ylabel("Total Loss GrIS / (Mt $\\cdot$ month$^{-1} \\cdot km^{2}$))", **fontstyle)
     ax.set_xlabel("Datetime", **fontstyle)
     thic_plot(ax)
 
